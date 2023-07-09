287a35b716f730a54fd4dc056b6f7c9010bd7c2f
Were_Woof <118316858+Were-Woof@users.noreply.github.com>
2023-07-09 14:50:28 +0800

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,98 @@
+import requests
+
+API_KEY = '' #This allows you to access the hypixel api. To get a key, go to https://developer.hypixel.net/ and follow the instructions.
+user = '' #This is the name of the user that your client is under
+path = fr'c:\Users\{user}\.lunarclient\offline\multiver\logs\latest.log' #If this isn't where your chat logs file is (and if you are using a client other than Lunar), please replace this with the correct path
+
+
+def check_stats(player):
+    r = requests.get(f'https://api.mojang.com/users/profiles/minecraft/{player}?').json()
+    try:
+        uuid = r['id']
+    except:
+        return False
+
+
+    guild_data = requests.get(f'https://api.hypixel.net/guild?key={API_KEY}&player={uuid}').json()
+
+    data = requests.get(f"https://api.hypixel.net/player?key={API_KEY}&uuid={uuid}").json()
+    try:
+        player_data = data["player"]["stats"]
+    except:
+        return False
+    try:
+        bedwars_data = player_data["Bedwars"]
+    except:
+        return False
+    try:
+        duels_data = player_data["Duels"]
+    except:
+        return False
+
+    try:
+        bedwars_star = data["player"]["achievements"]["bedwars_level"]
+    except:
+        bedwars_star = 0
+    try:
+        bw_final_deaths = bedwars_data["final_deaths_bedwars"]
+    except:
+        bw_final_deaths = 1
+    try:
+        bw_final_kills = bedwars_data["final_kills_bedwars"]
+    except:
+        bw_final_kills = 1
+    try:
+        bw_fkdr = bw_final_kills/bw_final_deaths
+    except:
+        bw_fkdr = 1
+
+    guild_data = guild_data['guild']
+
+    index = bedwars_star * (bw_fkdr**2)
+
+    if guild_data != None:
+        return False
+    
+    if index >= 2000:
+        return True
+    return False
+
+def find_players():
+    print('Starting..\n')
+
+    message = ''
+
+    with open(path, 'r+b') as file:
+        for i in file.readlines():
+            #print(i)
+            i = str(i)[42:-5]
+            
+            if i.startswith('Online Players'):
+
+                i = i.split(':')[1]
+                message = i
+
+        while ',' in message:
+
+            message = message.split(',')
+                            
+        for player in message:
+
+            if '[MVP++]' in player:
+
+                player = player[9:]
+
+            elif '[' in player:
+
+                player = player[8:]
+                                
+            else:
+                                    
+                player = player[1:]
+
+            if check_stats(player):
+                print(player)
+
+        print('\nFinished!')
+
+find_players()
