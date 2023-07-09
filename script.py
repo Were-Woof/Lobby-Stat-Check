51cb85efb0401584ecffd498d9f3a11c7d62f5cf
oDepleted <114532730+oDepleted@users.noreply.github.com>
2023-07-09 22:27:30 +1200

    Update script.py

--- script.py
+++ script.py
@@ -1,44 +1,50 @@
-import requests
 import os
-#print(os.getlogin())
-#print(os.getcwd())
+import re
+import requests
 
-API_KEY = '' #This allows you to access the hypixel api. To get a key, go to https://developer.hypixel.net/ and follow the instructions.
-#If this isn't where your chat logs file is (and if you are using a client other than Lunar), please replace this with the correct path
+
+# This allows you to access the hypixel api.
+# To get a key, go to https://developer.hypixel.net/ and follow the instructions.
+API_KEY = ''
+
+# Here we attempt to work out where your chat logs file is.
+# If this doesn't work, you will need to replace the path variable
+# with the correct value. Make sure to keep the 'fr' before the string.
 user_profile = os.getenv('USERPROFILE')
-path = fr'{user_profile}\.lunarclient\offline\multiver\logs\latest.log'
+path = fr'{user_profile}\.lunarclient\offline\multiver\logs\latest.log' 
 
 def check_stats(player):
-    r = requests.get(f'https://api.mojang.com/users/profiles/minecraft/{player}?').json()
+    res = requests.get(f'https://api.mojang.com/users/profiles/minecraft/{player}?').json()
     try:
-        uuid = r['id']
-    except:
+        uuid = res['id']
+    except KeyError:
         return False
 
+    headers = {"API-Key": API_KEY}
+    guild_data = requests.get(
+        f'https://api.hypixel.net/guild?player={uuid}',
+        headers=headers).json()
 
-    guild_data = requests.get(f'https://api.hypixel.net/guild?key={API_KEY}&player={uuid}').json()
+    data = requests.get(
+        f"https://api.hypixel.net/player?uuid={uuid}",
+        headers=headers).json()
 
-    data = requests.get(f"https://api.hypixel.net/player?key={API_KEY}&uuid={uuid}").json()
     try:
-        player_data = data["player"]["stats"]
-        bedwars_data = player_data["Bedwars"]
-        duels_data = player_data["Duels"]
-    except:
+        stats_data: dict = data.get('player').get('stats', {})
+    except (KeyError, ValueError):
         return False
+
+    bedwars_data = stats_data.get('Bedwars', {})
+    duels_data = stats_data.get('Duels', {})
 
     try:
         bedwars_star = data["player"]["achievements"]["bedwars_level"]
-    except:
+    except KeyError:
         bedwars_star = 0
 
-    bw_final_deaths = bedwars_data.get("final_deaths_bedwars", 0)
-    bw_final_deaths = 1
-    bw_final_kills = bedwars_data.get("final_kills_bedwars", 0)
-
-    try:
-        bw_fkdr = bw_final_kills / (bw_final_deaths or 1)
-    except:
-        bw_fkdr = 1
+    bw_final_kills = bedwars_data.get('final_kills_bedwars', 0)
+    bw_final_deaths = bedwars_data.get('final_deaths_bedwars', 0)
+    bw_fkdr = bw_final_kills / (bw_final_deaths or 1)
 
     guild_data = guild_data['guild']
 
@@ -51,44 +57,31 @@ def check_stats(player):
         return True
     return False
 
+
 def find_players():
     print('Starting..\n')
 
-    message = ''
-
     with open(path, 'r+b') as file:
-        for i in file.readlines():
-            #print(i)
-            i = str(i)[42:-5]
-            
-            if i.startswith('Online Players'):
+        for line_bytes in file.readlines():
+            line = str(line_bytes).split('[CHAT] ')[-1]  # Only message content
+            line = re.sub(r'\\[nrt]', '', line)  # Remove all escape characters
 
-                i = i.split(':')[1]
-                message = i
+            if line.startswith('Online Players'):
+                line = line.split(': ')[-1]  # Players only in message
+                players = line.split(', ')  # Split at comma and remove whitespace before name
 
-        while ',' in message:
+                for player in players:
+                    # Split name at end of rank and get last element (works for every rank including non)
+                    player = player.split('] ')[-1]
 
-            message = message.split(',')
-                            
-        for player in message:
-
-            if '[MVP++]' in player:
-
-                player = player[9:]
-
-            elif '[' in player:
-
-                player = player[8:]
-                                
-            else:
-                                    
-                player = player[1:]
-
-            if check_stats(player):
-                print(f'{player} meets requirements. TRUE')
-            else:
-                print(f'{player} does not meet requirements. FALSE')
+                    if check_stats(player):
+                        print(f'{player} meets requirements. TRUE')
+                    else:
+                        print(f'{player} does not meet requirements. FALSE')
 
         print('\nFinished!')
 
-find_players()
+try:
+    find_players()
+except KeyboardInterrupt:
+    exit(0)
